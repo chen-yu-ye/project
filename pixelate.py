b1bb9535eb3d698d888974a79df7f14d22994037
chen-yu-ye <71300533+chen-yu-ye@users.noreply.github.com>
2022-04-17 18:06:56 +0800

    Update pixelate.py

--- pixelate.py
+++ pixelate.py
@@ -1,15 +1,24 @@
 import cv2
-import numpy as np
-image = cv2.imread('download.jpg')
-start = [0,0] #upper left bound
-end = [image.shape[0],image.shape[1]] #lower right bound
-size = 15 #pixel sized
-end[0] = end[0]-((end[0]-start[0]) % size)#adjusting so that pixel is in the image.
-end[1] = end[1]-((end[1]-start[1]) % size)
-for i in range (start[0],end[0],size):
-    for j in range (start[1],end[1],size):
-        for k in range(size):
-            for l in range(size):
-                image[i+k][j+l] = image[i+size//2][j+size//2]
-cv2.imshow('Image', image)
 
+def pixel(x1,y1,x2,y2,image):
+    start = [x1,y1] #upper left bound
+    end = [x2,y2] #lower right bound
+    size = 15 #pixel sized
+    end[0] = end[0]-((end[0]-start[0]) % size)#adjusting so that pixel is in the image.
+    end[1] = end[1]-((end[1]-start[1]) % size)
+    for i in range (start[0],end[0],size):
+        for j in range (start[1],end[1],size):
+            for k in range(size):
+                for l in range(size):
+                    image[i+k][j+l] = image[i+size//2][j+size//2]
+    return image
+
+'''
+image = cv2.imread('download.jpg')
+x1 = 0
+y1 = 0
+x2 = image.shape[0]
+y2 = image.shape[1]
+image = pixel(x1,y1,x2,y2,image)
+cv2.imshow('Image', image)
+'''
